7f14fe5e0097ac37ec5d8cbada1a134c298007a6
Biagi <alessandro.biagi@sap.com>
2021-09-04 17:52:58 -0300

    Finished debugging received data.

--- managebp/handler.js
+++ managebp/handler.js
@@ -15,23 +15,14 @@ const manageBP = require('./manage-bp.js');
 module.exports = async function (event, context) {
     let msg;
     try {
-        //msg = JSON.parse(JSON.stringify(event.data));
         msg = JSON.parse(event.data);
-        console.log("msg 01:", msg);
     } catch (e) {
-        try {
-            msg = JSON.parse(event.data);
-            console.log("msg 02:", msg);
-        } catch (e) {
-            if (event.data) {
-                msg = event.data
-                console.log("msg 03:", msg);
-            } else {
-                return "Looks like there is some issue with the data format. Expected data format: JSON/String";
-            }
+        if (event.data) {
+            msg = event.data
+        } else {
+            return "Looks like there is some issue with the data format. Expected data format: JSON/String";
         }
     }
-    console.log("msg 04:", msg);
     const res = await manageBP.executeOperation(msg);
     return res;
 }
