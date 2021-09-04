b8e0646523ec622a1290214832872acd427c6aea
Biagi <alessandro.biagi@sap.com>
2021-09-04 15:52:08 -0300

    Debug received data.

--- managebp/handler.js
+++ managebp/handler.js
@@ -3,7 +3,7 @@
  * @typedef {import("@sap/faas").Faas.Event} Faas.Event
  * @typedef {import("@sap/faas").Faas.Context} Faas.Context
  */
- 
+
 const manageBP = require('./manage-bp.js');
 
 /**
@@ -11,21 +11,24 @@ const manageBP = require('./manage-bp.js');
   * @param {Faas.Context} context
   * @return {Promise<*>}
   */
- 
- module.exports = async function (event, context) {
-     let msg;
-     try {
+
+module.exports = async function (event, context) {
+    let msg;
+    try {
         msg = JSON.parse(JSON.stringify(event.data));
-     } catch (e) {
-         if (event.data) {
-             msg = event.data
-             console.log("msg", msg);
-         } else {
-             return "Looks like there is some issue with the data format. Expected data format: JSON/String";
-         }
-     }
-     console.log("msg:", msg);
-     const res = await manageBP.executeOperation(msg);
-     return res;
- }
- 
+    } catch (e) {
+        try {
+            msg = JSON.parse(event.data);
+        } catch (e) {
+            if (event.data) {
+                msg = event.data
+                console.log("msg", msg);
+            } else {
+                return "Looks like there is some issue with the data format. Expected data format: JSON/String";
+            }
+        }
+    }
+    console.log("msg:", msg);
+    const res = await manageBP.executeOperation(msg);
+    return res;
+}
